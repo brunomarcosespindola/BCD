abd3ade0ff856f758c157ab26a3f82fe3930df19
brunomarcosespindola <brunomarcosespindola@gmail.com>
2016-06-08 20:37:26 -0300

    Realizado inserção no banco de dados
    Colocado presidentes como professores e membros da academia como alunos.

---
+++ banco.sql
@@ -0,0 +1,104 @@
+#200.135.233.12
+
+drop table if exists processoMatricula;
+drop table if exists matricula;
+drop table if exists horario;
+drop table if exists preRequisito;
+drop table if exists disciplina;
+drop table if exists curso;
+drop table if exists professor;
+drop table if exists aluno;
+
+# aluno(*cpf,nome,senha)
+create table aluno(
+	id smallint unsigned auto_increment primary key,
+    cpf bigint unsigned,
+	nome varchar(50),
+	senha varchar(20)
+);
+
+# professor(*siape,nome)
+create table professor(
+	id smallint unsigned auto_increment primary key,
+	siape int unsigned,
+    nome varchar(50)
+);
+
+# curso(*codigo,nome)
+create table curso(
+	id tinyint unsigned auto_increment primary key,
+	codigo smallint unsigned,
+    nome varchar(100)
+);
+
+# disciplina(*codigo,fase,CH,CHmin,**cursoCodigo, **professorSiape)
+# cursoCodigo referencia curso (codigo)
+# professorSiape referencia professor(siape)
+create table disciplina(
+	id smallint unsigned auto_increment primary key,
+	codigo char(4),
+    nomeCompleto varchar(50),
+    fase tinyint unsigned,
+    CH smallint unsigned,
+    CHminima smallint unsigned,
+    cursoCodigo tinyint unsigned,
+    professorSiape smallint unsigned,
+    constraint fk_disciplina_cursoCodigo_curso_id 
+		foreign key (cursoCodigo) references curso(id),
+    constraint fk_disciplina_professorSiape_professor_id 
+		foreign key (professorSiape) references professor(id)
+);
+
+# PreRequisitos(***Disciplina,***PreRequisito)
+create table preRequisito(
+	id mediumint unsigned auto_increment primary key,
+	disciplina smallint unsigned,
+	preRequisito smallint unsigned,
+    constraint fk_preRequisito_disciplina_disciplina_id 
+		foreign key (disciplina) references disciplina(id),
+    constraint fk_preRequisito_preRequisito_disciplina_id 
+		foreign key (preRequisito) references disciplina(id)
+);
+
+# Horario(*Turno,*NumAula,*NumDia,*SemanaAB,**Disciplina,Semestre)
+# Disciplina referencia Discplina (CodCurso)
+create table horario(
+	id tinyint unsigned auto_increment primary key,
+    turno tinyint unsigned,
+    numAula tinyint unsigned,
+    numDia tinyint unsigned,
+    semanaAB tinyint unsigned,
+    codigoDisciplina smallint unsigned,
+    semestre smallint unsigned,
+    constraint fk_horario_codigoDisciplina_disciplina_id 
+		foreign key (codigoDisciplina) references disciplina(id)
+);
+
+# Matricula(**aluno,**curso,matricula)
+# aluno referencia aluno (cpf)
+# curso referencia curso (codigo)
+create table matricula(
+	id smallint unsigned auto_increment primary key,
+    aluno smallint unsigned,
+    curso tinyint unsigned,
+    matricula int unsigned,
+    constraint fk_matricula_aluno_aluno_id 
+		foreign key (aluno) references aluno(id),
+    constraint fk_matricula_curso_curso_id 
+		foreign key (curso) references curso(id)
+);
+
+# processoMatricula(***matricula,***disciplinca,semestre,concluido)
+# disciplina  referencia disciplina (codigo)
+# matricula referencia matricula(aluno,curso)
+create table processoMatricula(
+	id smallint unsigned auto_increment primary key,
+    matricula smallint unsigned,
+    disciplina smallint unsigned,
+    concluido tinyint unsigned,
+    semestre smallint unsigned,
+    constraint fk_processoMatricula_matricula_matricula_id
+		foreign key (matricula) references matricula(id),
+	constraint fk_processoMatricula_disciplina_disciplina_id
+		foreign key (disciplina) references disciplina(id)
+);
---
+++ disciplinas.sql
@@ -0,0 +1,86 @@
+# --------------------- CURSO -------------------------------------------
+insert into curso(codigo,nome)
+values ('290','Engenharia de Telecomunicações');
+#update curso set nome = 'Engenharia de Telecomunicações' where codigo = '1';
+# --------------------- DISCIPLINA ---------------------------------------
+#FASE 1
+insert into disciplina (codigo,nomeCompleto,fase,CH,CHminima,cursoCodigo,professorSiape)
+values ('DES','DESENHO TÉCNICO','1','36',NULL,'1','1'),
+	   ('CAL1','CÁLCULO I','1','72',NULL,'1','1'),
+       ('ELI','ELETRICIDADE E INSTRUMENTAÇÃO','1','72',NULL,'1','1'),
+       ('FSC1','FÍSICA I','1','72',NULL,'1','1'),
+       ('GAL','GEOMETRIA ANALÍTICA','1','54',NULL,'1','1'),
+       ('LOG','LÓGICA','1','36',NULL,'1','1'),
+       ('PJI1','PROJETO INTEGRADOR I','1','54',NULL,'1','1');
+#FASE 2
+insert into disciplina (codigo,nomeCompleto, fase,CH,CHminima,cursoCodigo,professorSiape)
+values ('QMC1','QUÍMICA GERAL','2','54',NULL,'1','2'),
+	   ('CAL2','CÁLCULO II','2','72',NULL,'1','2'),
+	   ('CIE1','CIRCUITOS ELÉTRICOS I','2','72',NULL,'1','2'),
+	   ('FSC2','FÍSICA II','2','72',NULL,'1','2'),
+	   ('ALG','ÁLGEBRA LINEAR','2','54',NULL,'1','2'),
+	   ('PRG1','PROGRAMAÇÃO I','2','72',NULL,'1','2');
+#FASE 3
+insert into disciplina (codigo,nomeCompleto, fase,CH,CHminima,cursoCodigo,professorSiape)
+values ('QMC2','CIÊNCIA E TECNOLOGIA DOS MATERIAIS','3','36',NULL,'1','3'),
+	   ('CAL3','CÁLCULO III','3','72',NULL,'1','3'),
+	   ('CIE2','CIRCUITOS ELÉTRICOS II','3','72',NULL,'1','3'),
+	   ('ELA1','ELETRÔNICA I','3','72',NULL,'1','3'),
+	   ('CIL','CIRCUITOS LÓGICOS','3','72',NULL,'1','3'),
+	   ('PRG2','PROGRAMAÇÃO II','3','72',NULL,'1','3');
+#FASE 4
+insert into disciplina (codigo,nomeCompleto, fase,CH,CHminima,cursoCodigo,professorSiape)
+values ('MEC','MECÂNICA DOS SÓLIDOS','4','36',NULL,'1','4'),
+	   ('CAL4','CÁLCULO IV (PARA TELECOMUNICAÇÕES)','4','72',NULL,'1','4'),
+	   ('RED1','REDES DE COMPUTADORES I','4','72',NULL,'1','4'),
+	   ('ELA2','ELETRÔNICA II','4','72',NULL,'1','4'),
+	   ('MIC','MICROPROCESSADORES','4','72',NULL,'1','4'),
+	   ('POO','PROGRAMAÇÃO ORIENTADA A OBJETOS','4','72',NULL,'1','4');
+#FASE 5
+insert into disciplina (codigo,nomeCompleto, fase,CH,CHminima,cursoCodigo,professorSiape)
+values ('MPQ','METODOLOGIA DE PESQUISA','5','36',NULL,'1','5'),
+	   ('EST','ESTATÍSTICA E PROBABILIDADE','5','54',NULL,'1','5'),
+	   ('RED2','REDES DE COMPUTADORES II','5','72',NULL,'1','5'),
+	   ('FSC3','FÍSICA III','5','90',NULL,'1','5'),
+	   ('SOP','SISTEMAS OPERACIONAIS','5','72',NULL,'1','5'),
+	   ('SIS1','SINAIS E SISTEMAS I','5','72',NULL,'1','5');
+#FASE 6
+insert into disciplina (codigo,nomeCompleto, fase,CH,CHminima,cursoCodigo,professorSiape)
+values ('FEN','FENÔMENOS DE TRANSPORTE','6','36',NULL,'1','6'),
+	   ('PRE','PROCESSOS ESTOCÁSTICOS','6','54',NULL,'1','6'),
+	   ('ANT','ANTENAS E PROPAGAÇÃO','6','72',NULL,'1','6'),
+	   ('STD','SISTEMAS DISTRIBUÍDOS','6','54',NULL,'1','6'),
+	   ('DLP1','DISPOSITIVOS LÓGICOS PROGRAMÁVEIS I','6','90',NULL,'1','6'),
+	   ('SIS2','SINAIS E SISTEMAS II','6','54',NULL,'1','6'),
+	   ('PJI2','PROJETO INTEGRADOR II','6','36',NULL,'1','6');
+#FASE 7
+insert into disciplina (codigo,nomeCompleto, fase,CH,CHminima,cursoCodigo,professorSiape)
+values ('PTG','COMUNICAÇÃO E EXPRESSÃO','7','36','1980','1','7'),
+	   ('RTX','REDES DE TRANSMISSÃO','7','72',NULL,'1','7'),
+	   ('MTG','MEIOS DE TRANSMISSÃO GUIADOS','7','72',NULL,'1','7'),
+	   ('DLP2','DISPOSITIVOS LÓGICOS PROGRAMÁVEIS II','7','54',NULL,'1','7'),
+	   ('PSD','PROCESSAMENTO DE SINAIS DIGITAIS','7','72',NULL,'1','7'),
+	   ('COM1','SISTEMAS DE COMUNICAÇÃO I','7','90',NULL,'1','7');       
+#FASE 8
+insert into disciplina (codigo,nomeCompleto, fase,CH,CHminima,cursoCodigo,professorSiape)
+values ('ADM','ADMINISTRAÇÃO PARA ENGENHARIA','8','36','1980','1','8'),
+	   ('CSF','COMUNICAÇÕES SEM FIO','8','72',NULL,'1','8'),
+	   ('BCD','BANCOS DE DADOS','8','54',NULL,'1','8'),
+	   ('STE','SISTEMAS EMBARCADOS','8','72',NULL,'1','8'),
+	   ('PTC','PROJETO DE PROTOCOLOS','8','54',NULL,'1','8'),
+	   ('COM2','SISTEMAS DE COMUNICAÇÃO 2','8','72',NULL,'1','8'),
+	   ('PJI3','PROJETO INTEGRADOR III','8','36',NULL,'1','8');
+#FASE 9
+insert into disciplina (codigo,nomeCompleto, fase,CH,CHminima,cursoCodigo,professorSiape)
+values ('ECO','ECONOMIA PARA ENGENHARIA','9','36','1980','1','9'),
+	   ('SUS','ENGENHARIA E SUSTENTABILIDADE','9','36',NULL,'1','9'),
+	   ('SMU','SISTEMAS MULTIMÍDIA','9','54',NULL,'1','9'),
+	   ('ADS','AVALIAÇÃO DE DESEMPENHO DE SISTEMAS','9','54',NULL,'1','9'),
+	   ('STC','SISTEMAS DE TELECOMUNICAÇÃO','9','72',NULL,'1','9'),
+	   ('CRF','CIRCUITOS DE RÁDIO-FREQUÊNCIA','9','72',NULL,'1','9'),
+	   ('TCC1','TRABALHO DE CONCLUSÃO DE CURSO I','9','36','2160','1','9');
+#FASE 10
+insert into disciplina (codigo,nomeCompleto, fase,CH,CHminima,cursoCodigo,professorSiape)
+values ('ESTO','ESTÁGIO OBRIGATÓRIO','10','360','2160','1','10'),
+	   ('TCC2','TRABALHO DE CONCLUSÃO DE CURSO II','10','108','2560','1','10');
+ 
